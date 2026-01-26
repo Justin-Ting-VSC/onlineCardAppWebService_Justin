7d74c02acb5d1ba0a261e013ab446c967d4dcae3
Justin-Ting-VSC <24041609@myrp.edu.sg>
2026-01-26 12:54:04 +0800

    Added Link

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const cors = require("cors");
 
 const allowedOrigins = [
   "http://localhost:3000",
-  // "https://YOUR-frontend.onrender.com"
+  "https://teams2-justin-c219.onrender.com"
 ];
 
 app.use(
